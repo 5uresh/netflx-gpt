8adb672d5b9d2d536aeaa9fb8a40acdd9d5ae54c
5uresh <suresh.oneforce@gmail.com>
2024-10-18 13:23:21 +0530

    Created new app - initia push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='text-3xl font-bold text-red-800'>Lets Build the new Project</p>
+    </div>
+  );
+}
+
+export default App;
