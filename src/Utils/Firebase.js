27f2a8f22feb2ec705986b058e4a9ae876d63841
5uresh <suresh.oneforce@gmail.com>
2024-10-21 13:00:28 +0530

    Firebase configured

---
+++ src/Utils/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBrO2ppVZwDWJb2zStL5F8TcP8v_kUJ4V8",
+  authDomain: "netflx-gpt.firebaseapp.com",
+  projectId: "netflx-gpt",
+  storageBucket: "netflx-gpt.appspot.com",
+  messagingSenderId: "929906302787",
+  appId: "1:929906302787:web:70e14dd1b2830ec57359f6",
+  measurementId: "G-XP18EYGRF8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
